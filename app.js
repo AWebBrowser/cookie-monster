99dc3f9caaf897ad0bd41d54176398a5cc01a909
Jan Armendariz-Bones <30916174+AWebBrowser@users.noreply.github.com>
2018-02-01 14:47:38 -1000

    Finished

---
+++ app.js
@@ -0,0 +1,14 @@
+function source(number){
+	if(number = one){
+		window.alert("source for image: http://moziru.com/images/jar-clipart-chocolate-chip-cookie-14.png");
+	}
+	if(number = two){
+		window.alert("source for image: https://vignette.wikia.nocookie.net/cookieclicker/images/5/50/GoldCookie.png/revision/latest?cb=20130825165158");
+	}
+	if(number = three){
+		window.alert("source for image: http://clipart-library.com/image_gallery2/Cookie-Download-PNG.png")
+	}
+	if(number = four){
+		window.alert("source for image: http://moziru.com/images/mm-clipart-cookie-1.jpg");
+	}
+}
